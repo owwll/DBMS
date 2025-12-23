712feb6e3e0695699521bb47beec5440a46d0d16
Dhruvin Vaghasiya <24ce137@charusat.edu.in>
2025-12-23 02:48:12 -0800

    Create supplier and additional tables from employee_137

--- Practical_2.sql
+++ Practical_2.sql
@@ -7,5 +7,10 @@ SELECT table_name
 DESC tablename;
 
 -- create new table with the help of the other table (create supplier table with existing table employee_137)
-CREATE table supplier AS SELECT * FROM employee_137;
+CREATE TABLE supplier AS SELECT * FROM employee_137;
 
+-- only create with 2 column of employee_137 table
+CREATE TABLE sup1 AS SELECT emp_no , emp_name FROM employee_137;
+
+-- only create stucture not copy data so we have to write such condition whitch is allways fails
+CREATE TABLE sup2_137 AS SELECT * FROM employee_137 WHERE (EMP_NO <0);
