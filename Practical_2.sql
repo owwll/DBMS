90841db4359fe2ddd418c745c4ee7e475ff58a86
Dhruvin Vaghasiya <24ce137@charusat.edu.in>
2026-01-20 02:01:19 -0800

    Update Practical_2.sql

--- Practical_2.sql
+++ Practical_2.sql
@@ -14,3 +14,15 @@ CREATE TABLE sup1 AS SELECT emp_no , emp_name FROM employee_137;
 
 -- only create stucture not copy data so we have to write such condition whitch is allways fails
 CREATE TABLE sup2_137 AS SELECT * FROM employee_137 WHERE (EMP_NO <0);
+
+-- Insert the data into sup2_137 from employee whose name is ‘Anita’ and verify.
+INSERT INTO sup2_137 SELECT * FROM employee_137 WHERE emp_name = 'Anita';
+
+-- Rename the table sup2 and verify
+RENAME sup2_137 to suplier2_137;
+DESC sup2_137;
+
+-- Destroy table sup1_137 with all the data and verify
+DROP TABLE sup1_137;
+DESC sup1_137;
+
